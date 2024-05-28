a47deee07d1dd55f1cccacc36426ad68ae593c32
liuyang <liuyang08035@gmail.com>
2024-05-28 15:23:46 +0800

    update

--- tools/plot_label_using_val.py
+++ tools/plot_label_using_val.py
@@ -6,9 +6,9 @@ current_dir = Path(__file__).absolute().parent
 model_dir = current_dir.parent.parent / "yolo_models"
 
 model = YOLOWorld(str(model_dir / "yolov8s-worldv2.pt"))
-model.set_classes(["person", "car", "truck", "boat", "airplane", "train", "bicycle", "tricycle"])
+model.set_classes(["tank"])
 args = {"data": "./data_plot_images/plot_data.yaml", "device": "0", "project": "plot_dataset",
-        "name": "tmp", "batch": 1, "imgsz": 3840, "split": "val", }
+        "name": "tmp", "batch": 1, "imgsz": 1280, "split": "val", }
 
 metrics = model.val(**args)
 
