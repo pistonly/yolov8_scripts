bda1f1bea5a82a2303761840bc934222b6b7cdb0
liuyang <liuyang08035@gmail.com>
2024-05-28 11:17:54 +0800

    update

--- predict_yolo-world.py
+++ predict_yolo-world.py
@@ -3,15 +3,15 @@ from ultralytics import YOLOWorld
 
 # models = ["yolov8s-worldv2", "yolov8m-worldv2", "yolov8l-worldv2", "yolov8x-worldv2",
 #           "yolov8s-world", "yolov8m-world", "yolov8l-world", "yolov8x-world"]
-models = ["yolov8x-worldv2",]
+models = ["yolov8s-worldv2-finetune-visdrone-1280", "yolov8s-worldv2",]
 
 for m in models:
     model = YOLOWorld(f'../yolo_models/{m}.pt')
 
     # args = {"data": "VisDrone.yaml", "project": "yolov8-visdrone",
     #         "device": "0", "batch": 1, 'augment': False, 'imgsz': 2016, 'name': 'val-yolov8l-640train-2016t'}
-    args = {"data": "datasets-config/silu__dataset_4k_2.yaml", "device": "0", "project": "yolo-world_test", "name": f"predict__dataset_4k__train__{m}-0",
-            "batch": 1, "imgsz": 3840, "split": "train", "source": "/data/silu/dataset_4k/images/train/",
+    args = {"data": "datasets-config/silu__dataset_4k_2.yaml", "device": "0", "project": "yolo-world_test", "name": f"predict__dataset_4k__sample__{m}-0",
+            "batch": 1, "imgsz": 3840, "split": "train", "source": "/home/liuyang/datasets/qiyuan/dataset_4k/sample/",
             "save": True}
     model.set_classes(["person", "car", "truck", "boat", "airplane", "train", "bicycle", "tricycle"])
     model.predict(**args)
--- train_hsh_yolov8_2.py
+++ train_hsh_yolov8_2.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 data = "./datasets-config/hsh_second.yaml"
-model = YOLO('../yolo_models/yolov8m.pt')
+model = YOLO('../yolo_models/yolov8n.pt')
 
 args = {"data": data, "epochs": 300, "project": "yolov8-hsh-second",
-        "imgsz": 1280, "name": "yolov8m", 'batch': 8}
+        "imgsz": 1280, "name": "yolov8n", 'batch': 16}
 results = model.train(**args)
 
 
